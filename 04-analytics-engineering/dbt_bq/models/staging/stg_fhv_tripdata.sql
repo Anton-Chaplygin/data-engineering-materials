8f98d4eeea61ffefc35d938de04c9c1eb61d0167
Anton Chaplygin <Jagerman@yandex.ru>
2024-02-25 19:53:28 +0000

    removing limit 100

--- 04-analytics-engineering/dbt_bq/models/staging/stg_fhv_tripdata.sql
+++ 04-analytics-engineering/dbt_bq/models/staging/stg_fhv_tripdata.sql
@@ -20,6 +20,6 @@ where extract(Year from cast(pickup_datetime as timestamp)) = 2019
 -- dbt build --select <model.sql> --vars '{'is_test_run: false}'
 {% if var('is_test_run', default=true) %}
 
-  limit 100
+--   limit 100
 
 {% endif %}
